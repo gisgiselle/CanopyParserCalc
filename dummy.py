10b4a23058e88a29fad013669655c5b83c5fccbf
gisgiselle <brengimolina@gmail.com>
2022-08-30 18:53:05 -0500

    Update dummy.py

--- dummy.py
+++ dummy.py
@@ -1,2 +1,22 @@
 import calculator
+import calculator
+
+grammar = """"
+
+prog <- (expr NEWLINE*)
+expression <- addition / factor
+addition <- factor (_ "+" _factor)+
+substract <- factor (_ "-" _factor)+
+division <- factor (_ "/" _factor)+
+multiplication <- factor (_ "*" _factor)+
+
+factor <- number / par_expression
+number <- "0" / [1-9] [0-9]*
+par_expression <- "(" expression ")"
+"""
+tree = calculator.parse(grammar, "2+1")
+
+for node in tree.elements:
+    print (node.offset, node.text)
+
 
